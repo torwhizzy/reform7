8c8b927fdca1e8fe7036d5c275423be961e9d168
torwhizzy <39039062+torwhizzy@users.noreply.github.com>
2021-05-24 17:38:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en"> 
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title>Reform-7</title>
+  <meta name="description" content="Reform-7" >
+  
+  
+  <meta property="og:site_name" content="Reform-7" >
+  <meta property="og:type" content="article" >
+  <meta property="og:title" content="Reform-7" >
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta content="yes" name="apple-mobile-web-app-capable">
+
+
+<link rel="canonical" href="index.html" />
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  "name": "reform-7",
+  "url": "https://reform-7.org"
+}
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:400,400i,600,600i,700,700i|Barlow+Semi+Condensed:400,400i,600,600i,700,700i|Lato:300,300i,400,400i,700,700i|Montserrat:300,300i,400,400i,700,700i" rel="stylesheet"> 
+<link rel="stylesheet" href="_assets/fonts/icons/styledf19.css?t=20180427142120%20?>" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<link rel="stylesheet" href="_assets/css/bootstrapc462.css?t=20180822173258" />
+<link rel="stylesheet" href="_assets/css/mainf671.css?t=20200416232632" />
+
+<link rel="icon" href="_assets/img/logos/reform logo.jpg">
+<link rel="apple-touch-icon-precomposed" href="_assets/img/favicons/favicon-1525e1f.png?v=2">
+<meta name="msapplication-TileColor" content="#0a3a53">
+<meta name="msapplication-TileImage" content="/_assets/img/favicons/favicon-144.png?v=2">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8B39WQ');</script>
+<!-- End Google Tag Manager --></head>
+
+<body data-campus="" class="index-layout">
+
+<div id="wrap">
+<?php
+ require_once'includes/header.php';
+?>
+
+<div id="billboard" class="dark hasvideo hastext" >
+<div id="bigvideo"><video id="billboardvideo" autoplay playsinline loop muted><source src="https://media.istockphoto.com/videos/legs-of-a-business-man-wearing-black-shoes-and-business-suit-walking-video-id1137526383"></video></div><div class="text"><div class="container"><div class="inner"><a class="btn btn-primary btn-lg" target="_blank" href="online/index.html" type="link" rel="noopener">WATCH Online</a></p>
+</div>
+</div>
+</div><!--end .text--><a href="#" id="billboardDown"><i class="icon icon-arrow-down3"></i></a>
+</div><!--end #billboard-->
+
+<!-- About us section -->
+<section class="goal">
+  <h1>Our Goal - Our Vision</h1>
+  <p>By the Holy Spirit, is to create a SYNERGY of the Word of Faith, and the Apostolic, <br> Prophetic, and Prayer streams to raise REFORMERS to influence society every day in Africa.</p>
+  <div class="row">
+      <div class="goal-column">
+          <h3>SYNERGY</h3>
+          <p>SYNERGY is defined here as the interaction of these different streams of the Holy Spirit to produce a multiplied effect in the society greater than the sum of their individual effects. This multiplied effect is called REFORMATION!</p>
+      </div>
+      <div class="goal-column">
+          <h3>REFORMATION </h3>
+          <p>REFORMATION is whereby God makes a people (or person) and their way of life ‘right’ from worse, corrupt, or evil state to good, blessed and prosperous state that is aligned with His original plan and purpose for them.</p>
+      </div>
+      <div class="goal-column">
+      <h3>REFORMERS </h3>
+      <p>
+      In this age, God has chosen to work through His REFORMERS to influence the seven mountains of the society in Africa: Government, Religion, Family, Media, Art/Sport/Entertainment, Education, and Economy.
+  </p>
+  </div>
+  </div>
+  <div class="clear_both">&nbsp;</div>
+</section>
+
+<section id="csec-home-join-us-this-weekend" class="content-section">
+  <div class="content-section-inner py-8 py-md-11 dark" style=" background-color: #f0f0f0;">
+  <div class="container">
+    <div class="content-text">
+      <h1 style="text-align: center; color: #323234;">
+        <strong>Join Our sunday Services</strong></h1>
+<p></p>
+<p style="text-align: center; margin-bottom: 0px; margin-top: 0px;"><a class="btn btn-primary btn-lg btn-block" href="weekend/index.html" type="link">Join now</a></p></div></div></div><!--end content section--></section><!--end content section-->
+
+<section id="csec-home-latest-message" class="content-section">
+  <div class="content-section-inner py-8 py-md-11" >
+    <div class="container">
+      <div class="content-text">
+        <div>
+      <div class="widget media-display-full-right">
+        <div class="column hasinfo">
+          <div class="info"><h4 class="mb-4">Latest Message</h4><h5><a href="message/4-grow-in-grace-part-1.html">Title of the message here</a></h5><p class="summary mb-4">A short discription of the message will be displayed here.</p><p class="more mb-0">
+              <a href="messages/index.html" class="btn event-btn ">More Messages</a>
+              <a href="messages/index5c5d.html?series=whose-friend-are-you-series" class="btn event-btn">Watch Series</a>
+            </p>
+          </div>
+          </div>
+
+        <div class="column hasmedia">
+          <div class="div-embed" style="background-image: url(../aa7302164449a7c1f252-288152864d5ed7e130010f369eb2459b.ssl.cf2.rackcdn.com/uploaded/w/0e12346056_1620101632_whose-friend-are-you-series-video-thumbnail-4-grow-in-grace-part-1.jpg);">
+          <iframe src="https://player.vimeo.com/video/547007367?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="#4 Grow in Grace">
+          </iframe>
+        </div>
+      </div>
+     
+      </div>
+      </div>
+    </div>
+  </div>
+  </div><!--end content section-->
+</section><!--end content section-->
+
+<section id="csec-home-join-us-this-weekend" class="content-section">
+  <div class="content-section-inner py-8 py-md-11 dark" style=" background-color: #323234;">
+  <div class="container">
+    <div class="content-text">
+      <h1 style="text-align: center; color: #fff;">
+        <strong>Reform-7 ongoing project- first Edition</strong></h1>
+<p></p>
+<p style="text-align: center; margin-bottom: 0px; margin-top: 0px;"><a class="btn btn-primary btn-lg btn-block" href="#" type="link">view project</a></p></div></div>
+</div><!--end content section-->
+</section><!--end content section-->
+<section id="csec-home-serving-opportunities" class="content-section">
+<div class="content-section-inner py-8 py-md-11" style=" background-color: #f0f0f0;">
+<div class="container">
+<div class="widget media-display-full-left">
+  <div class="column hasmedia"><div class="div-embed" style="background-image: url(_assets/img/layouts/pst.jpg);"></div></div><div class="column hasinfo"><div class="info"><h4>Workers</h4>
+<h2 style="margin-bottom: 10px;">Be a part of our work force!</h2>
+<p style="margin-bottom: 10px;">Sow back into the kingdom of God. You can make a difference in someone's life by joining any of our workers team today!</p>
+<p><a class="btn btn-default" href="outreach-serve-opportunities/index.html" type="link">Join an Outreach Team</a><a class="btn btn-default" href="#" type="link">Learn More</a></p></div></div></div>
+</div>
+</div><!--end content section-->
+</section><!--end content section-->
+
+<!-- ----- Event section----- -->
+<section class="event">
+  <h1>Our Latest Activities</h1>
+  <p>Don't miss out. Follow all our latest activities below</p>
+  <div class="row">
+  <div class="event-col">
+      <img src="_assets/img/lightgallery/image1.jpg" alt="">
+          <h3>Holy Ghost service</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro corporis minima facere aperiam ab, molestias consectetur quae nihil! Deserunt repellat illum ipsa iure maxime recusandae dicta deleniti, aut alias culpa.</p>
+          <a href="" class="event-btn">view more...</a>
+      </div>
+  <div class="event-col">
+      <img src="_assets/img/lightgallery/basketball.png" alt="">
+          <h3>End of the month crusade</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat sequi est cum optio facilis deleniti suscipit labore molestiae at repellat. Cumque nam esse perferendis numquam, delectus et nihil pariatur consectetur.</p>
+          <a href="" class="event-btn">view more...</a>
+      </div>
+  <div class="event-col">
+      <img src="_assets/img/lightgallery/image2.jpg" alt="">
+          <h3>Sunday June 12, Covenant Service</h3>
+          <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus hic eaque, veniam, ipsa id architecto, quaerat ipsum eveniet cupiditate quisquam voluptatibus porro sequi provident sed quis nostrum repudiandae dolores non.
+      </p>
+      <a href="" class="event-btn">view more...</a>
+      </div>
+  </div>
+</section>
+
+
+<section id="csec-home-prayer-support-and-follow-pastor-dale" class="content-section">
+  <div class="content-section-inner py-8 py-md-11" >
+    <div class="container-fluid">
+      <div class="content-text">
+        <div>
+          <div class='widget featured-columns no-sec-padding row no-gutters align-items-center' style = "width:100%; margin-left:0;">
+            <article class='col-12 col-md dark'>
+        <a href="#">
+          <div class='image' style='background-color: #323234;'></div>
+          <div class='info'>
+          <div class='inner'>
+  <h3>Prayer Support</h3>
+  <p>Submit a prayer below, our team would love to pray for you!</p></div><p class='more'><span class='btn btn-primary'>Share Your Prayer</span></p>
+</div>
+</a>
+</article>
+<article class='col-12 col-md dark'>
+  <a href='https://www.instagram.com/torwhizzy/' target='_blank'><div class='image' style='background-color: #323234;'></div><div class='info'><div class='inner'><h3>Follow DR. DRAY</h3><p>In these uncertain times, get daily biblical encouragement!</p></div><p class='more'><span class='btn btn-primary'>Follow On Instagram</span></p></div></a></article></div></div>
+</div>
+</div>
+</div><!--end content section-->
+</section><!--end content section-->
+<section id="csec-home-ways-to-give" class="content-section"><div class="content-section-inner py-8 py-md-11" ><div class="container"><div class="content-text"><h4 style="text-align: center;">Your giving helps</h4>
+<h2 style="text-align: center;">Make an eternal impact</h2>
+<p></p>
+<h5 style="margin-bottom: 10px; text-align: center;">
+  <span>All through Scripture, we discover that obedience to God's commands and principles is the pathway to blessing. We demonstrate trust and depth in God by how we allocate the resources He provides us.<br />
+</span>
+<a href="give/index.html">Learn more.</a>
+</h5>
+<p></p>
+<p style="text-align: center; margin-bottom: 0px;"><span><a class="btn btn-default" target="_blank" href="#" type="link" rel="noopener">Give Now</a></span></p></div></div>
+</div><!--end content section-->
+</section><!--end content section-->
+<?php
+ require_once'includes/footer.php';
+?>
+
+</div><!--end #wrap-->
+<script src="_assets/js/lib/jquery.min.js"></script>
+<script src="../my.ekklesia360.com/Clients/monkcms-dev.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbrpuchUaTNE9xQjwEAqOIDxnVG3dmQDc"></script>
+<script src="_assets/js/lib/infobox.js"></script><script src="_assets/js/monk/layouts/index.js"></script>
+<script src="_assets/js/lib/plugins.js"></script>
+<script src="_assets/js/lib/jarallax.min.js"></script>
+<script src="_assets/js/lib/jarallax-element.min.js"></script>
+<script src="_assets/js/monk/main.js"></script>
+<script src="_assets/js/lib/SmoothScroll.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+  <!-- Font Awesome 5 links-->
+  <script src="https://kit.fontawesome.com/fddf5c0916.js" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+var addthis_share = {
+     url_transforms : {
+          shorten: {
+               twitter: 'bitly'
+          }
+     }, 
+     shorteners : {
+          bitly : {} 
+     }
+}
+</script>
+<script type="text/javascript" src="../s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b7c2cc86bae9183" async="async"></script><!-- Start Google Search Console Code -->
+<meta name="google-site-verification" content="r_H4ADcsfCG2Fm1WrfkkD0jp0rKV-UU4iIxM1zW03jo" />
+<meta name="google-site-verification" content="ZrBmia0B-_UWB-lVqAUSWyKvOGiwco73ruzJ3x10WA0" />
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-16815301-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-16815301-2');
+</script>
+<!-- End Google Search Console Code -->
+
+<!-- Start Facebook Pixel Code -->
+<!-- <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'../connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '854246254721107'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=854246254721107&amp;ev=PageView&amp;noscript=1"/>
+</noscript> -->
+<!-- End Facebook Pixel Code -->
+</body>
+</html>
