bf0e2a83c4823b141cef5a2d7a0245d9275f4e51
torwhizzy <39039062+torwhizzy@users.noreply.github.com>
2021-05-25 13:59:37 +0100

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,72 +1,61 @@
-
+<?php $dir = "http://localhost/reform7";  ?>
 <footer class="footer-distributed">
 
 <div class="container first_class">
   <div class="row">
-    <div class="col-md-4 col-sm-6">
-      <h3>BE Part Of Our Daily Devotionals</h3>
-      <p>Get all the latest messages from us. Sign up for our newsletter today.</p>
+    <div class="col-12 col-lg-4 ">
+      <h3 class="mb-4">BE Part Of Our Daily Devotionals</h3>
+      <p class="mb-2">Get all the latest messages from us. Sign up for our newsletter today.</p>
     </div>
-    <div class="col-md-4 col-sm-6">
+    <div class="col-12 col-lg-4">
     <form class="newsletter">
        <input type="text" placeholder="Email Address"> 
         <button class="newsletter_submit_btn" type="submit"><i class="fa fa-paper-plane"></i></button>  
     </form>
     
     </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="col-md-12">
+    <div class="col-12 col-lg-4">
+      <div class="col-md-12 d-flex align-items-center justify-content-center">
         <div class="standard_social_links">
       <div>
-        <li class="round-btn btn-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
+        <li class="round-btn btn-facebook d-flex align-items-center justify-content-center"><a href="#"><i class="fab fa-facebook-f"></i></a>
   
         </li>
-        <li class="round-btn btn-linkedin"><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+        <li class="round-btn btn-linkedin d-flex align-items-center justify-content-center"><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
   
         </li>
-        <li class="round-btn btn-twitter"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+        <li class="round-btn btn-twitter d-flex align-items-center justify-content-center"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
   
         </li>
-        <li class="round-btn btn-instagram"><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+        <li class="round-btn btn-instagram d-flex align-items-center justify-content-center"><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
   
         </li>
-        <li class="round-btn btn-whatsapp"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+        <li class="round-btn btn-whatsapp d-flex align-items-center justify-content-center"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
   
         </li>
       </div>
     </div>  
       </div>
         <div class="clearfix"></div>
-      <div class="col-md-12"><h3 style="text-align: right;">Stay Connected</h3></div>
+      <div class="col-md-12"><h3>Stay Connected</h3></div>
       </div>
   </div>
 </div>
-
+<br />
 <div class="footer-left">
-
    
-
   <p class="footer-links">
-    <a href="#" class="link-1">Home</a>
-    
-    <a href="other pages/blog.php">Blog</a>
-
-    <a href="#">Project</a>
-
-    <a href="other pages/about.php">About Us</a>
-    
+  <a href="<?php echo $dir; ?>">Home</a>
+  <a href="<?php echo $dir.'/other-pages/blog.php'; ?>">Blog</a>
+    <a href="<?php echo $dir.'/other-pages/project.php'; ?>">Project</a>
+    <a href="<?php echo $dir.'/other-pages/about.php'; ?>">About</a>
     <a href="#">Faq</a>
-    
-    <a href="other-pages/contact.php">Contact</a>
+    <a href="<?php echo $dir.'/other-pages/contact.php'; ?>">Contact</a>
 </p>
-
-    <div class="logo"><a href="#"><img src="_assets/img/layouts/banner.jpg" alt=""></a></div>
+    <div class="logo mx-auto d-inline-block"><a href="<?php echo $dir; ?>" title="Reform-7"><img src="<?php echo $dir.'/_assets/img/logos/reform logo2.jpg'; ?>" alt=""/></a></div>
 </div>
-
 <div class="footer-center">
-
     
-
     <p class="footer-about">
       <span>About the church</span>
       Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
@@ -75,27 +64,21 @@
   <div class="sec-title-style1 text-center max-width">
       <div class="text"><div class="decor-left"><span></span></div><p>Reform-7</p><div class="decor-right"><span></span></div></div>
   </div>
-
 </div>
-
 <div class="footer-right">
-
   <div>
     <i class="fa fa-map-marker"></i>
     <p><span>4B Idak Okpo Lane, Uyo</span> Akwa Ibom, Nigeria</p>
 </div>
-
 <div>
     <i class="fa fa-phone"></i>
     <p>(+234)070 1497 0207</p>
 </div>
-
 <div>
     <i class="fa fa-envelope"></i>
     <p><a href="mailto:support@reform-7">support@reform7.com</a></p>
 </div>
    
 </div>
-
 <div class="copyright"> Copyright 2021 | All Rights Reserved by limelight technology company Ltd.</div>
 </footer>
