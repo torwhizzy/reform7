2c10973fa43187ebacd9fd9e75db80acd06fbbe3
torwhizzy <39039062+torwhizzy@users.noreply.github.com>
2021-05-29 12:21:22 +0100

    Add files via upload

--- _assets/js/monk/main.js
+++ _assets/js/monk/main.js
@@ -111,6 +111,15 @@ function selectors(){
 	
 }
 
+$(document).ready(function(){
+	$(".dropdown").hover(function(){
+		var dropdownMenu = $(this).children(".dropdown-menu");
+		if(dropdownMenu.is(":visible")){
+			dropdownMenu.parent().toggleClass("open");
+		}
+	});
+});
+
 
 /*============================================
  * Global
